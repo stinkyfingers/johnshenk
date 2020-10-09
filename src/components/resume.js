c95cc4ea7e4d6ff6506bd1ed1c583625a699feb4
John Shenk <john_shenk@hotmail.com>
2020-10-09 15:18:59 -0500

    switches to querystring

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,11 +4,25 @@ import ResumeText from './resumetext';
 const Plain = React.lazy(() => import('./plaincss'));
 const Normal = React.lazy(() => import('./normalcss'));
 
+const queryString = (queryString) => {
+  // note: does not support multiple values per key
+  const str = queryString.replace('?', '', 1);
+  const queries = {};
+  str.split('&').map(s => {
+    const arr = s.split('=');
+    if (arr.length !== 2) return null;
+    return queries[arr[0]] = arr[1];
+  });
+  return queries;
+};
+
 const Resume = () => {
+  const queries = queryString(window.location.search);
+
   return (
     <div className="resume">
       <React.Suspense fallback={<ResumeText />}>
-        {window.location.pathname === '/plain' ? <Plain /> : <Normal />}
+        {queries.format === 'plain' ? <Plain /> : <Normal />}
         <ResumeText />
       </React.Suspense>
     </div>
